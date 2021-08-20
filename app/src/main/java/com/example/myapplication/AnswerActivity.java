02fe35948e7b44a70a7f4c2ceafbf65043873b2a
Eztekto <iztekto@gmail.com>
2021-08-21 01:20:05 +0300

    Попытка сделать 12ое заданаие

--- app/src/main/java/com/example/myapplication/AnswerActivity.java
+++ app/src/main/java/com/example/myapplication/AnswerActivity.java
@@ -2,12 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AnswerActivity extends AppCompatActivity {
-
+    private TextView answerTextView;
+    private boolean isAnswerTrue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+
+        isAnswerTrue = getIntent().getBooleanExtra("answer", false);
+
+        answerTextView = findViewById(R.id.answerTextView);
+        answerTextView.setText(isAnswerTrue?R.string.yes:R.string.no);
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,56 +24,80 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question5, false)
     };
     int questionIndex = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("SYSTEM INFO: ", "Вызван метод onCreate()");
         setContentView(R.layout.activity_main);
-        if (savedInstanceState != null)
-            questionIndex = savedInstanceState.getInt("questionIndex", 0);
 
+        if(savedInstanceState != null){
+            questionIndex = savedInstanceState.getInt("questionIndex");
+        }
         yesBtn = findViewById(R.id.yesBtn);
         noBtn = findViewById(R.id.noBtn);
         questionTextView = findViewById(R.id.questionTextView);
+        showAnswer = findViewById(R.id.showAnswer);
         questionTextView.setText(questions[questionIndex].getQuestionText());
-
         yesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (questions[questionIndex].isAnswerTrue())
-                    Toast.makeText(MainActivity.this, "Правильно!", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Не правильно!", Toast.LENGTH_SHORT).show();
-                questionIndex = (questionIndex + 1) % questions.length;
-                questionTextView.setText(questions[questionIndex].getQuestionText());
+                checkAnswer(true);
             }
         });
-
         noBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!questions[questionIndex].isAnswerTrue())
-                    Toast.makeText(MainActivity.this, "Правильно!", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Не правильно!", Toast.LENGTH_SHORT).show();
-                questionIndex = (questionIndex + 1) % questions.length;
-                questionTextView.setText(questions[questionIndex].getQuestionText());
+                checkAnswer(false);
             }
-
         });
         showAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AnswerActivity.class);
-                Intent.putExtra("answer", questions)
+                intent.putExtra("answer", questions[questionIndex].isAnswerTrue());
+                startActivity(intent);
             }
         });
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putInt("questionIndex", questionIndex);
+    public void checkAnswer(boolean btn){
+        if((questions[questionIndex].isAnswerTrue() && btn) || (!questions[questionIndex].isAnswerTrue() && !btn))
+            Toast.makeText(MainActivity.this, "Правильно!", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Не правильно!", Toast.LENGTH_SHORT).show();
+        questionIndex = (questionIndex+1)%questions.length;
+        questionTextView.setText(questions[questionIndex].getQuestionText());
+    }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d("SYSTEM INFO: ", "Вызван метод onStart()");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d("SYSTEM INFO: ", "Вызван метод onResume()");
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d("SYSTEM INFO: ", "Вызван метод onSaveInstanceState()");
+        savedInstanceState.putInt("questionIndex", questionIndex);
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("SYSTEM INFO: ", "Вызван метод onPause()");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d("SYSTEM INFO: ", "Вызван метод onStop()");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("SYSTEM INFO: ", "Вызван метод onDestroy()");
     }
 }
